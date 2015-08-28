5a64553a96622105dd1305975aab983b142366bd
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-08-28 10:44:10 -0700

    add app routes and views for view all brands and view all stores.

--- app/app.php
+++ app/app.php
@@ -4,22 +4,34 @@
     require_once __DIR__."/../src/Brand.php";
 
     $app = new Silex\Application();
-    $app['debug'] = true;
+    $app["debug"] = true;
 
-    $server = 'mysql:host=localhost;dbname=shoes';
-    $username = 'root';
-    $password = 'root';
+    $server = "mysql:host=localhost;dbname=shoes";
+    $username = "root";
+    $password = "root";
     $DB = new PDO($server, $username, $password);
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
-        'twig.path' => __DIR__.'/../views'));
+        "twig.path" => __DIR__."/../views"));
 
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
     // Home Route: Choose Brand or Store.
-    $app->get("/", function() use($app){
-        return $app['twig']->render('index.html.twig');
+    $app->get("/", function() use($app) {
+        return $app["twig"]->render("index.html.twig");
+    });
+
+    // All Brands Route
+    $app->get("/brands", function() use ($app) {
+        $brands = Brand::getAll();
+        return $app["twig"]->render("brands.html.twig", array("brands" => $brands));
+    });
+
+    // All Stores Route
+    $app->get("/stores", function() use ($app) {
+        $stores = Store::getAll();
+        return $app["twig"]->render("stores.html.twig", array("stores" => $stores));
     });
 
     return $app;
