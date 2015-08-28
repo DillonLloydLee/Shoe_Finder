1806bab6174890f171341c63287af01bcd266dc0
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-08-28 12:59:50 -0700

    add addBrand and getBrands to Store class.  Tests all pass.

--- src/Store.php
+++ src/Store.php
@@ -63,11 +63,23 @@
         }
 
         function addBrand($brand) {
-
+            $GLOBALS['DB']->exec("INSERT INTO brands_stores (brands_id, stores_id) VALUES ({$brand->getId()}, {$this->getId()});");
         }
 
         function getBrands() {
+            $returned_brands = $GLOBALS['DB']->query("SELECT brands.* FROM
+                stores JOIN brands_stores ON (stores.id = brands_stores.stores_id)
+                         JOIN brands ON (brands_stores.brands_id = brands.id)
+                         WHERE stores.id = {$this->getId()};");
 
+            $brands = array();
+            foreach($returned_brands as $brand) {
+                $name = $brand['name'];
+                $id = $brand['id'];
+                $new_brand = new Brand($name, $id);
+                array_push($brands, $new_brand);
+            }
+            return $brands;
         }
 
 
