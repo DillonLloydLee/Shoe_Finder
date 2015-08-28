e4f2e21b210032a033aa1406b10abc3e374b9bf4
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-08-28 11:28:25 -0700

    write tests for rename, remove, find.

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -33,5 +33,50 @@
             $this->assertEquals([$test_brand, $test_brand2], $result);
         }
 
+        function test_rename() {
+            $name = "Roooobok";
+            $test_brand = new Brand($name);
+            $test_brand->save();
+
+            $new_name = "Reebok";
+            $test_brand->rename($new_name);
+
+            $result = $test_brand->getName();
+
+            $this->assertEquals("Reebok", $result);
+        }
+
+        function test_remove() {
+            $name = "Reebok";
+            $test_brand = new Brand($name);
+            $test_brand->save();
+
+            $name2 = "Nike";
+            $test_brand2 = new Brand($name2);
+            $test_brand2->save();
+
+            $test_brand2->remove();
+
+            $result = Brand::getAll();
+
+            $this->assertEquals($test_brand, $result);
+        }
+
+        function test_find() {
+            $name = "Reebok";
+            $test_brand = new Brand($name);
+            $test_brand->save();
+
+            $name2 = "Nike";
+            $test_brand2 = new Brand($name2);
+            $test_brand2->save();
+
+            $search_id = $test_brand2->getId();
+
+            $result = Brand::find($search_id);
+
+            $this->assertEquals($test_brand2, $result);
+        }
+
     }
 ?>
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -20,17 +20,62 @@
         }
 
         function test_save_and_getAll() {
-            $name = "Reebok";
+            $name = "Dirty Shoes";
             $test_store = new Store($name);
             $test_store->save();
 
-            $name2 = "Nike";
+            $name2 = "Clean Shoes";
             $test_store2 = new Store($name2);
             $test_store2->save();
 
             $result = Store::getAll();
 
             $this->assertEquals([$test_store, $test_store2], $result);
+        }
+
+        function test_rename() {
+            $name = "POOOOP Shoes";
+            $test_store = new Store($name);
+            $test_store->save();
+
+            $new_name = "Poop Shoes";
+            $test_store->rename($new_name);
+
+            $result = $test_store->getName();
+
+            $this->assertEquals("Poop Shoes", $result);
+        }
+
+        function test_remove() {
+            $name = "Dirty Shoes";
+            $test_store = new Store($name);
+            $test_store->save();
+
+            $name2 = "Clean Shoes";
+            $test_store2 = new Store($name2);
+            $test_store2->save();
+
+            $test_store2->remove();
+
+            $result = Store::getAll();
+
+            $this->assertEquals($test_store, $result);
+        }
+
+        function test_find() {
+            $name = "Dirty Shoes";
+            $test_store = new Store($name);
+            $test_store->save();
+
+            $name2 = "Clean Shoes";
+            $test_store2 = new Store($name2);
+            $test_store2->save();
+
+            $search_id = $test_store2->getId();
+
+            $result = Store::find($search_id);
+
+            $this->assertEquals($test_store2, $result);
         }
 
     }
